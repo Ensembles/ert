358e03acc952ddbe53172888739b76fa3b7e4bfe
Joakim Hove <joakim.hove@gmail.com>
2013-12-16 16:18:56 +0100

    Added import ert.util.libutil to libconfig.py

--- devel/python/python/ert/config/libconfig.py
+++ devel/python/python/ert/config/libconfig.py
@@ -13,7 +13,7 @@
 #   
 #  See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
 #  for more details. 
-
+import ert.util.libutil
 
 import ert.cwrap.clib as clib
 lib = clib.ert_load("libconfig.so")
