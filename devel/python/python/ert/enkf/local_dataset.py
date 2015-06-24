e25b8d20bfa903cfc779f9426e52a3efb2cec353
Ariel Almendral Vazquez <ariel@nr.no>
2015-06-24 14:46:02 +0200

    ERT-ZZZ: Python interface for local ministep (cont)

--- devel/python/python/ert/enkf/local_dataset.py
+++ devel/python/python/ert/enkf/local_dataset.py
@@ -2,37 +2,41 @@ from ert.cwrap import BaseCClass, CWrapper
 from ert.enkf import ENKF_LIB
 
 
-class Localdataset(BaseCClass):
+class LocalDataset(BaseCClass):
 
     def __init__(self, name):
         assert isinstance(name, str)
 
-        c_pointer = Localdataset.cNamespace().alloc(name)
-        super(Localdataset, self).__init__(c_pointer)
+        c_pointer = LocalDataset.cNamespace().alloc(name)
+        super(LocalDataset, self).__init__(c_pointer)
                  
     def getName(self):
-        """ @rtype: str """
-        return Localdataset.cNamespace().name(self)
+        return LocalDataset.cNamespace().name(self)
     
     def getActiveList(self, key):
-        """ @rtype: str """
-        return Localdataset.cNamespace().active_list(self, key)
+        return LocalDataset.cNamespace().active_list(self, key)
 
+    def addNode(self, key):
+        return LocalDataset.cNamespace().add_node(self, key)
+    
     def free(self):
-        Localdataset.cNamespace().free(self)
+        LocalDataset.cNamespace().free(self)
 
 
 
 cwrapper = CWrapper(ENKF_LIB)
-cwrapper.registerType("local_dataset", Localdataset)
-cwrapper.registerType("local_dataset_obj", Localdataset.createPythonObject)
-cwrapper.registerType("local_dataset_ref", Localdataset.createCReference)
+cwrapper.registerType("local_dataset", LocalDataset)
+cwrapper.registerType("local_dataset_obj", LocalDataset.createPythonObject)
+cwrapper.registerType("local_dataset_ref", LocalDataset.createCReference)
 
-Localdataset.cNamespace().alloc          = cwrapper.prototype("c_void_p local_dataset_alloc(char*)")
-Localdataset.cNamespace().free           = cwrapper.prototype("void local_dataset_free(local_dataset)")
-Localdataset.cNamespace().name           = cwrapper.prototype("char* local_dataset_get_name(local_dataset)")
-Localdataset.cNamespace().active_list    = cwrapper.prototype("active_list_ref local_dataset_get_node_active_list(local_dataset, char*)")
+LocalDataset.cNamespace().alloc          = cwrapper.prototype("c_void_p local_dataset_alloc(char*)")
+LocalDataset.cNamespace().free           = cwrapper.prototype("void local_dataset_free(local_dataset)")
+LocalDataset.cNamespace().name           = cwrapper.prototype("char* local_dataset_get_name(local_dataset)")
+LocalDataset.cNamespace().active_list    = cwrapper.prototype("active_list_ref local_dataset_get_node_active_list(local_dataset, char*)")
+LocalDataset.cNamespace().add_node       = cwrapper.prototype("void local_dataset_add_node(local_dataset, char*)")
 
+                                                                                 
+                                                                                 
 
 
 
--- devel/python/python/ert/enkf/local_ministep.py
+++ devel/python/python/ert/enkf/local_ministep.py
@@ -1,5 +1,5 @@
 from ert.cwrap import BaseCClass, CWrapper
-from ert.enkf import ENKF_LIB, LocalObsdata, LocalObsdataNode
+from ert.enkf import ENKF_LIB, LocalObsdata, LocalObsdataNode, LocalDataset
 
 class LocalMinistep(BaseCClass):
 
@@ -16,10 +16,11 @@ class LocalMinistep(BaseCClass):
     def attachObsset(self, obs_set):
         assert isinstance(obs_set, LocalObsdata)
         LocalMinistep.cNamespace().attach_obsset(self,obs_set)
-     
-#     def createObsData(self, obs_data_key):
-#         LocalMinistep.cNamespace().create_updatestep(self, obs_data_key)  
-#         return LocalMinistep.cNamespace().get_updatestep(self, obs_data_key)     
+        
+    def attachDataset(self, dataset):
+        assert isinstance(dataset, LocalDataset)
+        LocalMinistep.cNamespace().attach_dataset(self,dataset)
+  
         
     def getLocalObsData(self):
         return LocalMinistep.cNamespace().get_local_obs_data(self)
@@ -42,6 +43,6 @@ LocalMinistep.cNamespace().add_node            = cwrapper.prototype("void local_
 LocalMinistep.cNamespace().get_local_obs_data  = cwrapper.prototype("local_obsdata_ref local_ministep_get_obsdata(local_ministep)")
 LocalMinistep.cNamespace().free                = cwrapper.prototype("void local_ministep_free(local_ministep)")
 LocalMinistep.cNamespace().attach_obsset       = cwrapper.prototype("void local_ministep_add_obsdata(local_ministep,local_obsdata)")
+LocalMinistep.cNamespace().attach_dataset      = cwrapper.prototype("void local_ministep_add_dataset(local_ministep,local_dataset)")
 LocalMinistep.cNamespace().name                = cwrapper.prototype("char* local_ministep_get_name(local_ministep)")
-
 
