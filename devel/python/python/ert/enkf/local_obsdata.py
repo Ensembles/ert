37604b7b0d6cde393a4a24685c9fb5bc95239ddc
Ariel Almendral Vazquez <ariel@nr.no>
2015-06-30 11:18:34 +0200

    ERT-ZZZ: Ert script for local config (Cont)

--- devel/python/python/ert/enkf/local_obsdata.py
+++ devel/python/python/ert/enkf/local_obsdata.py
@@ -31,20 +31,7 @@ class LocalObsdata(BaseCClass):
         while cur < len(self):
            yield self[cur]
            cur += 1
-
-           
-    def addNode(self, node):
-        """ @rtype: bool """
-        assert isinstance(node, LocalObsdataNode)
-        node.convertToCReference(self)
-        already_exists_node_for_key = LocalObsdata.cNamespace().add_node(self, node)
-        return already_exists_node_for_key
-
-    
-    def addObsVector(self , obs_vector):
-        self.addNode( obs_vector.createLocalObs() )
-
-        
+                    
     def __contains__(self, item):
         """ @rtype: bool """
         if isinstance(item, str):
@@ -53,6 +40,14 @@ class LocalObsdata(BaseCClass):
             return LocalObsdata.cNamespace().has_node(self, item.getKey())
 
         return False
+    
+    
+    def addNode(self, node):
+        """ @rtype: bool """
+        assert isinstance(node, LocalObsdataNode)
+        node.convertToCReference(self)
+        already_exists_node_for_key = LocalObsdata.cNamespace().add_node(self, node)
+        return already_exists_node_for_key
 
     def getName(self):
         """ @rtype: str """
@@ -68,14 +63,14 @@ cwrapper.registerType("local_obsdata", LocalObsdata)
 cwrapper.registerType("local_obsdata_obj", LocalObsdata.createPythonObject)
 cwrapper.registerType("local_obsdata_ref", LocalObsdata.createCReference)
 
-LocalObsdata.cNamespace().alloc    = cwrapper.prototype("c_void_p local_obsdata_alloc(char*)")
-LocalObsdata.cNamespace().free     = cwrapper.prototype("void local_obsdata_free(local_obsdata)")
-LocalObsdata.cNamespace().size     = cwrapper.prototype("int local_obsdata_get_size(local_obsdata)")
-LocalObsdata.cNamespace().has_node = cwrapper.prototype("bool local_obsdata_has_node(local_obsdata, char*)")
-LocalObsdata.cNamespace().add_node = cwrapper.prototype("bool local_obsdata_add_node(local_obsdata, local_obsdata_node)")
+LocalObsdata.cNamespace().alloc     = cwrapper.prototype("c_void_p local_obsdata_alloc(char*)")
+LocalObsdata.cNamespace().free      = cwrapper.prototype("void local_obsdata_free(local_obsdata)")
+LocalObsdata.cNamespace().size      = cwrapper.prototype("int local_obsdata_get_size(local_obsdata)")
+LocalObsdata.cNamespace().has_node  = cwrapper.prototype("bool local_obsdata_has_node(local_obsdata, char*)")
+LocalObsdata.cNamespace().add_node  = cwrapper.prototype("bool local_obsdata_add_node(local_obsdata, local_obsdata_node)")
 LocalObsdata.cNamespace().iget_node = cwrapper.prototype("local_obsdata_node_ref local_obsdata_iget(local_obsdata, int)")
-LocalObsdata.cNamespace().get_node = cwrapper.prototype("local_obsdata_node_ref local_obsdata_get(local_obsdata, char*)")
-LocalObsdata.cNamespace().name     = cwrapper.prototype("char* local_obsdata_get_name(local_obsdata)")
+LocalObsdata.cNamespace().get_node  = cwrapper.prototype("local_obsdata_node_ref local_obsdata_get(local_obsdata, char*)")
+LocalObsdata.cNamespace().name      = cwrapper.prototype("char* local_obsdata_get_name(local_obsdata)")
 
 
 
