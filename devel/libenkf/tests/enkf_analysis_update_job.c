f1040856369684caec1f2631f72f664563c61ca7
Joakim Hove <joakim.hove@gmail.com>
2015-09-18 10:11:08 +0200

    ERT-941: White space change

--- devel/libenkf/tests/enkf_analysis_update_job.c
+++ devel/libenkf/tests/enkf_analysis_update_job.c
@@ -1,19 +1,19 @@
 /*
-   Copyright (C) 2013  Statoil ASA, Norway. 
-    
-   The file 'enkf_analysis_update_job.c' is part of ERT - Ensemble based Reservoir Tool. 
-    
-   ERT is free software: you can redistribute it and/or modify 
-   it under the terms of the GNU General Public License as published by 
-   the Free Software Foundation, either version 3 of the License, or 
-   (at your option) any later version. 
-    
-   ERT is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE.   
-    
-   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html> 
-   for more details. 
+   Copyright (C) 2013  Statoil ASA, Norway.
+
+   The file 'enkf_analysis_update_job.c' is part of ERT - Ensemble based Reservoir Tool.
+
+   ERT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   ERT is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.
+
+   See the GNU General Public License at <http://www.gnu.org/licenses/gpl.html>
+   for more details.
 */
 #include <stdlib.h>
 #include <stdbool.h>
@@ -48,7 +48,7 @@ void test_update_new_case(const char * config_file , const char * job_file) {
   ert_test_context_install_workflow_job( test_context , "JOB" , job_file );
   test_assert_true( ert_test_context_run_worklow_job( test_context , "JOB" , args) );
   stringlist_free( args );
-  
+
   ert_test_context_free( test_context );
 }
 
@@ -62,7 +62,7 @@ void test_update_new_case_step(const char * config_file , const char * job_file)
   ert_test_context_install_workflow_job( test_context , "JOB" , job_file );
   test_assert_true( ert_test_context_run_worklow_job( test_context , "JOB" , args) );
   stringlist_free( args );
-  
+
   ert_test_context_free( test_context );
 }
 
@@ -79,7 +79,7 @@ void test_update_new_case_step_selected(const char * config_file , const char * 
   ert_test_context_install_workflow_job( test_context , "JOB" , job_file );
   test_assert_true( ert_test_context_run_worklow_job( test_context , "JOB" , args) );
   stringlist_free( args );
-  
+
   ert_test_context_free( test_context );
 }
 
@@ -88,7 +88,7 @@ void test_update_new_case_step_selected(const char * config_file , const char * 
 int main(int argc , char ** argv) {
   const char * config_file = argv[1];
   const char * job_file = argv[2];
-  
+
   test_update_default( config_file , job_file);
   test_update_new_case( config_file , job_file );
   test_update_new_case_step( config_file , job_file );
