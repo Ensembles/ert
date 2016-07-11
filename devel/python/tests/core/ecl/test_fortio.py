caa482793b2134fa7ca66e60f64d5ea5f12a2bf9
Joakim Hove <joakim.hove@gmail.com>
2016-07-11 12:25:46 +0200

    Fixed copyright text.

--- devel/python/tests/core/ecl/test_fortio.py
+++ devel/python/tests/core/ecl/test_fortio.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #  Copyright (C) 2011  Statoil ASA, Norway.
 #
-#  The file 'kw_test.py' is part of ERT - Ensemble based Reservoir Tool.
+#  This file is part of ERT - Ensemble based Reservoir Tool.
 #
 #  ERT is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
