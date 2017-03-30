55de86c5b2c9140afd238837b5add1993ed19414
Pål Grønås Drange <PGDR@statoil.com>
2017-03-30 15:17:45 +0200

    added and refactor local_dataset test and geo tests

---
+++ python/tests/core/geometry/test_geo_region.py
@@ -0,0 +1,16 @@
+from ert.geo import GeoRegion, GeoPointset
+from ert.test import ExtendedTestCase, TestAreaContext
+
+
+class GeoRegionTest(ExtendedTestCase):
+
+    def test_init(self):
+        ps = GeoPointset()
+        gp = GeoRegion(ps)
+        print(repr(gp))
+        self.assertEqual(0, len(gp))
+
+    def test_repr(self):
+        ps = GeoPointset()
+        gp = GeoRegion(ps)
+        self.assertTrue(repr(gp).startswith('GeoRegion'))
