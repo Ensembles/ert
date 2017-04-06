c79105f516262f44a6494eea8a91f0c41c9361b0
Joakim Hove <joakim.hove@gmail.com>
2017-04-06 15:21:09 +0200

    Fixed build configuration - tests had been skipped

--- libert_util/tests/ert_util_abort_gnu_tests.c
+++ libert_util/tests/ert_util_abort_gnu_tests.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <ert/util/util.h>
 #include <ert/util/test_util.h>
-#include <ert/util/test_util_abort.h>
+#include <ert/util/test_util.h>
 
 
 void      test_assert_util_abort(const char * function_name , void (void *) , void * arg);
